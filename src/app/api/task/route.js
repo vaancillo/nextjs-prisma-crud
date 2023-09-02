4816c357de0db02d35fddbb11126a9d82889c2c1
vancillo <yaredbane@gmail.com>
2023-09-02 11:58:09 -0600

    next rest api & finishing the CRUD

--- src/app/api/task/route.js
+++ src/app/api/task/route.js
@@ -1,5 +1,20 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
-export function GET() {
-    return NextResponse.json('creando tareas')
+export async function GET() {
+    // consulta a base de datos
+    const task = await prisma.task.findMany()
+    return NextResponse.json(task)
+}
+
+export async function POST(request) {
+    const {title, description} = await request.json()
+    // inserta en la bd el nuevo registro
+    const newTask = await prisma.task.create({
+        data: {
+           title,
+           description
+        },
+    })
+    return NextResponse.json(newTask)
 }
