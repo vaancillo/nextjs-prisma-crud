5db8723909cb1469ff7cdf2c323d2aeb840eb639
vancillo <yaredbane@gmail.com>
2023-09-02 15:52:10 -0600

    update task & fixing files

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,36 +1,82 @@
 "use client"
-import {useRouter} from 'next/navigation'
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-function NewPage() {
+function NewPage({ params }) {
   const router = useRouter();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    if(params.id) {
+      fetch(`/api/task/${params.id}`)
+      .then(res => res.json())
+      .then(data => {
+        setTitle(data.title)
+        setDescription(data.description)
+      });
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    const title = e.target.title.value
-    const description = e.target.description.value
-    // console.log(title, description);
-    const res = await fetch('/api/task', {
-      method: 'post',
-      body: JSON.stringify({title, description}),
-      headers: {
-        'Content-Type': "application/json",
-      }
-    })
-    const data = await res.json()
-    console.log(data)
-  
-    router.push('/')
-  }
+    e.preventDefault();
+
+    if(params.id) {
+      
+      const res = await fetch(`/api/task/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify({title, description}),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      const data = await res.json()
+      console.log('data', data);
+    } else {
+      const res = await fetch('/api/task', {
+        method: 'POST',
+        body: JSON.stringify({ title, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await res.json();
+    }
+    router.refresh()
+    router.push('/');
+  };
+
   return (
     <div className="h-screen flex justify-center items-center">
       <form className="bg-slate-800 p-10" onSubmit={handleSubmit}>
-        <label htmlFor="title" className="font-bold text-sm">Titulo de la tarea</label>
-        <input id='title' type="text" className="border border-gray-400 p-2 mb-4 w-full text-black" placeholder="Titulo"/>
-        <label htmlFor="description" className="font-bold text-sm">Descripcion de la tarea</label>
-        <textarea rows="3" id="description" className="border border-gray-400 p-2 mb-4 w-full text-black" placeholder="Describe tu tarea"></textarea>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">crear</button>
+        <label htmlFor="title" className="font-bold text-sm">
+          Titulo de la tarea
+        </label>
+        <input
+          id="title"
+          type="text"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Titulo"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="description" className="font-bold text-sm">
+          Descripcion de la tarea
+        </label>
+        <textarea
+          rows="3"
+          id="description"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Describe tu tarea"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+        ></textarea>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          crear
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
-export default NewPage
+export default NewPage;
